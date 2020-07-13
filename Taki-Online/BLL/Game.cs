4e7147d7edc63746a7a7470efda097005445a865
itai <itailustig@gmail.com>
2020-07-14 01:55:00 +0300

    added project
    -added Game classes and color enum
    -added ui blank project

---
+++ Taki-Online/BLL/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace BLL
+{
+    public enum Color
+    {
+        none,
+        red,
+        green,
+        blue,
+        yellow
+    }
+    public class Game
+    {
+        private Stack<Card> deck;
+        private Stack<Card> pile;
+        private List<Player> players;
+        private int penelty;
+        private bool isOpenTaki;
+
+    }
+    public abstract class Card
+    {
+        private Color color;
+
+        public abstract bool CanPutOn(Card card);
+        public abstract 
+    }
+    public class Player
+    {
+        private List<Card> hand;
+        public void Draw(Game game)
+        {
+
+        }
+    }
+}
