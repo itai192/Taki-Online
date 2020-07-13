9861b725489ea8e597f1e61000e075fbef43fc7c
itai <itailustig@gmail.com>
2020-07-14 02:42:15 +0300

    added things in game

--- Taki-Online/BLL/Game.cs
+++ Taki-Online/BLL/Game.cs
@@ -16,23 +16,57 @@ namespace BLL
         private Stack<Card> deck;
         private Stack<Card> pile;
         private List<Player> players;
-        private int penelty;
+        private int turn;//turn
+        public bool order { get; set; }//positive or negative relative to the order
+        public int penelty {get; set;}//extra cards when you draw
         private bool isOpenTaki;
+        public Card TakeCardFromDeck()
+        {
+            if(deck.Count==0)
+            {
+                Reshuffle();
+            }
+            return deck.Pop();
+        }
+        public void NextTurn()
+        {
+            turn = (order ? (turn + 1) : (turn - 1 + players.Count)) % players.Count;
+        }
+        private void Reshuffle()
+        {
+
+        }
 
     }
     public abstract class Card
     {
         private Color color;
 
-        public abstract bool CanPutOn(Card card);
-        public abstract 
+        public virtual bool CanPutOn(Card card)//if you can put card, on this
+        {
+            if(card.color==Color.none)
+            {
+                return true;
+            }
+            return this.color == card.color;
+        }
+        public virtual void Play(Game game)
+        { 
+        }
     }
     public class Player
     {
         private List<Card> hand;
+        
         public void Draw(Game game)
         {
-
+            int drawAmount = 1 + game.penelty;
+            game.penelty = 0;
+            for (int i = 0; i < drawAmount; i++)
+            {
+                hand.Add(game.TakeCardFromDeck());
+            }
         }
     }
+
 }
