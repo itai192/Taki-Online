48d558a94dc37b9836adc56fe1b196a9a950ea94
itai192 <itailustig@gmail.com>
2020-09-09 08:37:08 +0300

    added db helper and dal helper

---
+++ Taki-Online/DAL/DBHelper.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace DAL
+{
+    public class DBHelper
+    {
+        //Constants 
+        public const int WRITEDATA_ERROR = -1;
+
+        //class member variables 
+        private OleDbConnection conn;
+        private string provider;
+        private string source;
+        private bool connOpen;
+
+        //Construct the object with a provided provider and source 
+        public DBHelper(string provider, string source)
+        {
+            this.provider = provider;
+            this.source = source;
+            this.conn = new OleDbConnection(BuildConnString());
+            this.connOpen = false;
+        }
+
+        //This function builds the connection string to be used to open connection.
+        private string BuildConnString()
+        {
+            return String.Format(@"Provider={0};Data Source={1};", this.provider, this.source);
+        }
+        //This function is closing the connection unless it is already closed! 
+        public void CloseConnection()
+        {
+            if (!this.connOpen)
+            {
+                this.OpenConnection();
+            }
+
+
+            if (this.connOpen)
+            {                 
+                    conn.Close();
+                    connOpen = false;
+            }
+        }
+        //Open connection to the database.
+        public void OpenConnection()
+        {
+            try
+            {
+                this.conn.Open();
+                connOpen = true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+        //Execute UPDATE or INSERT sql commands and return number of rows affected.         //return WRITEDATA_ERROR on failure
+        public int WriteData(string sql)
+        {
+            int ret = WRITEDATA_ERROR;
+            if (!this.connOpen)
+            {
+                this.OpenConnection();
+            }
+
+
+            if (this.connOpen)
+            {
+                OleDbDataReader rd = null;
+                try
+                {
+                    OleDbCommand cmd = new OleDbCommand(sql, conn);
+                    rd = cmd.ExecuteReader();
+                    ret = rd.RecordsAffected;
+                }
+                catch(Exception ex)
+                {
+                    ret = WRITEDATA_ERROR;
+                }
+            }
+            return ret;
+        }
+        //Execute SELECT sql commands and return a reference to an OleDbDataReader         
+        //if execution fails return null
+        public OleDbDataReader ReadData(string sql)
+        {
+            OleDbDataReader rd = null;
+            if (!this.connOpen)
+            {
+                this.OpenConnection();
+            }
+
+
+            if (this.connOpen)
+            {
+                try
+                {
+                OleDbCommand cmd = new OleDbCommand(sql, conn);
+                rd = cmd.ExecuteReader();
+                }
+                catch(Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return rd; 
+        }
+        //This function should be used for inserting a single record into a table in the database with an autonmuber key. the format of the sql must be          
+        //INSERT INTO <TableName> (Fields...) VALUES (values...)         
+        //the function return the autonumber key generated for the new record or WRITEDATA_ERROR if fail
+        public int InsertWithAutoNumKey(string sql)
+        {
+            int newID=WRITEDATA_ERROR;
+            if(!this.connOpen)
+            {
+                this.OpenConnection();
+            }
+            
+                
+            if (this.connOpen)
+            {
+                if (this.WriteData(sql) != WRITEDATA_ERROR)
+                {
+                    OleDbDataReader rd = ReadData(@"SELECT @@Identity");
+                    if (rd != null)
+                    {
+                        
+                        while (rd.Read())
+                        {
+                            newID = (int)rd[0];
+                        }
+                    }
+                }
+            }
+            return newID;
+        }
+        //This function reads from the database a data table fully cached in memory using a standard SQL SELECT statement.         
+        //The function returns the data table or null on failure. 
+        public DataTable GetDataTable(string sql)
+        {
+            try
+            {
+                DataTable tbl = new DataTable();
+                OleDbDataReader rd = ReadData(sql);
+                tbl.Load(rd);
+                return tbl;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //This function reads from the database a data set fully cached in memory using an array of standard SQL SELECT statements.         
+        //The function returns the data set or null on failure. The table names inside the dataset are sql1, sql2,... 
+        public DataSet GetDataSet(string[] sql)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+
+                for (int i = 0; i < sql.Length; i++)
+                {
+                    OleDbDataAdapter da = new OleDbDataAdapter(sql[i], this.conn);
+                    da.Fill(ds, "sql" + (i + 1));
+                }
+                return ds;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Taki-Online/DAL/DalHelper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace DAL
+{
+  public class DalHelper
+    {  
+        //note: you dont really need to read the following comment its mostly a joke(still true though)
+        //Have you ever been exhausted by those annoying sql senteses? and when you are done with them
+        //you would usually need to go to that depressing DBHelper in order to connect the two,
+    // WELL NO MORE - Dal Helper is here to make your life better, easier and filled with joy
+    // just provide us with the right sql and return! Yes exactly! DAL commands will only take 2 rows
+    // from now on and it is all because DALHelper is just Sooooooooo amazing 
+    // NO MORE Closeconnectiosn! the dal helper does it for you! 
+    //~Alon Metuky's speech.
+    
+        public static string provider;
+        public static string source;
+        static DBHelper h; //only the dal helper is using the dbhelper
+        public static void SetSource(string path)
+        {
+            source = path;
+        }
+        public static void SetProvider(string prov)
+        {
+            provider = prov;
+        }
+        public static void CreateDBHelper()
+        {
+            h = new DBHelper(provider, source);
+        }
+        public static int Insert(string sql) //works for all insert sql that also needs to create an int key and returns that key
+        {
+            int id = h.InsertWithAutoNumKey(sql);
+            h.CloseConnection();
+            return id;
+        }
+        public static DataRow SelectRow(string sql)  //works for all select sql that selects a single row and returns that row
+        {
+            try
+            {
+                DataRow row = h.GetDataTable(sql).Rows[0];
+                h.CloseConnection();
+                return row;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                h.CloseConnection();
+            }
+
+        }
+        public static bool Update(string sql)//works for all update sql and checks if the db changed, if it did than the update worked and returns true, else false
+        {
+           int num = h.WriteData(sql);
+            h.CloseConnection();
+            if(num ==-1)
+           {
+              return false;
+           }
+              return true;
+        }
+        public static DataTable SelectTable(string sql)//works for all select sql that selects a table and returns that table
+        {
+            DataTable Table = h.GetDataTable(sql);
+            h.CloseConnection();
+            return Table;
+        }
+        public static bool insertWithoutCreatingID(string sql)//works for all insert sql that doesnt need to create an auto key.
+        {
+            int num = h.WriteData(sql); 
+            h.CloseConnection();
+             if (num == -1)
+            { return false; }
+            return true;
+        }
+        public static bool IsExist(string sql)//gets a select sql for a row and checks if such row exists
+        { 
+            DataTable exist = h.GetDataTable(sql);
+            h.CloseConnection();
+            if(exist.Rows.Count==0)
+                {
+                    return false;
+                }
+                return true;
+        }
+    }
+}
