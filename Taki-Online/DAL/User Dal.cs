3e49a78bcbfd180d9e0c309eb6adabbae4e92c01
itai192 <itailustig@gmail.com>
2020-09-10 11:50:19 +0300

    fixed file issues

---
+++ Taki-Online/DAL/User Dal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class UserDal
+    {
+        /// <summary>
+        /// adds a user
+        /// </summary>
+        /// <returns>user ID</returns>
+        public static int AddUser()
+        {
+            string sql = "";
+            return DalHelper.Insert();
+        }
+    }
+}
