a722c48d53a03cd7b3c226477c7d3d1ff5a18919
itai <itailustig@gmail.com>
2021-03-09 02:21:32 +0200

    Revert "nothing"
    This reverts commit d4958fbe0b975d043ff7c0018fcac8e6ce9abe4f.

--- Taki-Online/UI/Game.aspx.cs
+++ Taki-Online/UI/Game.aspx.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -79,47 +78,4 @@ namespace UI
             Pile.LoadCard();
         }
     }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using BLL;
-using UI.Custom_Controls;
-namespace UI
-{
-    public partial class Game : System.Web.UI.Page
-    {
-        BLL.Game game;
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            UpdatePanel1.Update();
-        }
-
-        protected void TryUse(object sender, CardEventArgs e)
-        {
-            if (e.card.CanBePutOn(game.leadingCard))
-            {
-                Statuslbl.Text = "Good";
-                ((UI.Custom_Controls.Card)sender).PutCard();
-            }
-            else
-                Statuslbl.Text = "Can't put that card";
-        }
-
-        protected void UpdatePanel1_Load(object sender, EventArgs e)
-        {
-            game = (BLL.Game)Application["Game"];
-            Card.card = new BLL.NumberCard(Color.red, 4);
-            Pile.card = game.leadingCard;
-            Deck.card = new BLL.Reverse(Color.yellow);
-            Card2.card = new BLL.NumberCard(Color.yellow, 9);
-            Card.Click += TryUse;
-            Card2.Click += TryUse;
-            
-        }
-    }
->>>>>>> Stashed changes
 }
