bad89953be33a348dce074d2b58897f89ecbdc18
itai <itailustig@gmail.com>
2021-03-30 16:54:42 +0300

    did nothing

--- Taki-Online/UI/CreateGame.aspx.designer.cs
+++ Taki-Online/UI/CreateGame.aspx.designer.cs
@@ -13,5 +13,32 @@ namespace UI
 
     public partial class CreateGame
     {
+
+        /// <summary>
+        /// SearchPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel SearchPanel;
+
+        /// <summary>
+        /// GameText control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox GameText;
+
+        /// <summary>
+        /// SearchBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SearchBtn;
     }
 }
